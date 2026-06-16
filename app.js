160084ec79f14c3b4b6274a0b36d4488cf837eb3
abhiigna-c <abhiigna1077@gmail.com>
2026-06-16 23:30:32 +0530

    fixed functionality

--- app.js
+++ app.js
@@ -1,41 +1,105 @@
 let gameSequence = [];
 let userSequence = [];
+
 let level = 0;
 let started = false;
-let btns = ["btn-red", "btn-blue", "btn-green", "btn-yellow"];
-let h2 = document.querySelector("h2");
-document.addEventListener("keypress", function() {
-    if (!started) {
-      started = true;
-      console.log("Game started!");
 
-      levelUp();
-    }
+let btns = ["red", "blue", "green", "yellow"];
+
+let h2 = document.querySelector("h2");
+
+// Start game
+document.addEventListener("keypress", function () {
+  if (!started) {
+    started = true;
+    levelUp();
+  }
 });
 
+// Generate next sequence
 function levelUp() {
-    level++;
-    h2.textContent = `Level ${level}`;
+  userSequence = [];
 
-    let randomIndex = Math.floor(Math.random() * 4);
-    let randomColor = btns[randomIndex];
-    let randomBtn = document.querySelector(`.${randomColor}`);
-    console.log(randomColor);
-    btnFlash(randomBtn);
+  level++;
+  h2.textContent = `Level ${level}`;
+
+  let randomIndex = Math.floor(Math.random() * btns.length);
+  let randomColor = btns[randomIndex];
+
+  gameSequence.push(randomColor);
+
+  let randomBtn = document.querySelector(`#${randomColor}`);
+  btnFlash(randomBtn);
+
+  console.log("Game Sequence:", gameSequence);
 }
 
+// Flash game button
 function btnFlash(btn) {
   btn.classList.add("flash");
-  setTimeout(function() {
+
+  setTimeout(() => {
     btn.classList.remove("flash");
   }, 500);
 }
 
-function btnPress() {
-  console.log("Button pressed!");
+// Flash user click
+function userFlash(btn) {
+  btn.classList.add("userFlash");
+
+  setTimeout(() => {
+    btn.classList.remove("userFlash");
+  }, 250);
 }
 
+// Check latest answer
+function checkAnswer(currentIndex) {
+  if (userSequence[currentIndex] === gameSequence[currentIndex]) {
+    // User completed current level
+    if (userSequence.length === gameSequence.length) {
+      setTimeout(() => {
+        levelUp();
+      }, 1000);
+    }
+  } else {
+    h2.innerHTML = `Game Over! Your score was <b>${level}</b><br>Press any key to restart`;
+
+    document.body.classList.add("flash");
+
+    setTimeout(() => {
+      document.body.classList.remove("flash");
+    }, 200);
+
+    resetGame();
+  }
+}
+
+// Handle button click
+function btnPress() {
+  let btn = this;
+
+  userFlash(btn);
+
+  let userColor = btn.id;
+  userSequence.push(userColor);
+
+  let currentIndex = userSequence.length - 1;
+
+  checkAnswer(currentIndex);
+}
+
+// Reset game
+function resetGame() {
+  started = false;
+  level = 0;
+  gameSequence = [];
+  userSequence = [];
+  
+}
+
+// Add click listeners
 let buttons = document.querySelectorAll(".btn");
-for(btn of buttons) {
+
+for (let btn of buttons) {
   btn.addEventListener("click", btnPress);
 }
